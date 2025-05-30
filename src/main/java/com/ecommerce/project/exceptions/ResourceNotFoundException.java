6e3f213ad06161e9580d7d4bd3f574cf4af2f67b
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-05-30 19:50:32 +0530

    setup product model

---
+++ src/main/java/com/ecommerce/project/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    String resourceName;
+    String filed;
+    String filedName;
+    Long filedId;
+
+    public ResourceNotFoundException(String filed, String resourceName, Long filedId) {
+        super(String.format("%s not found with %s: %d",resourceName,filed,filedId));
+        this.filed = filed;
+        this.resourceName = resourceName;
+        this.filedId = filedId;
+    }
+
+    public ResourceNotFoundException(String filedName, String resourceName, String filed, Long filedId) {
+        super(String.format("%s not found with %s: %s",resourceName,filed,filedName));
+        this.filedName = filedName;
+        this.resourceName = resourceName;
+        this.filed = filed;
+
+    }
+    public ResourceNotFoundException(){
+
+    }
+}
---
+++ src/main/java/com/ecommerce/project/service/ProductServicesImpl.java
@@ -0,0 +1,14 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServicesImpl implements ProductService{
+
+    @Override
+    public ProductDTO addProduct(Product product,Long productId){
+
+    }
+}
