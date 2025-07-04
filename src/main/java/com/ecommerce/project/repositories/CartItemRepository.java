7d222ecb8d724491d675c152dbcb507b1c082cc5
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-07-04 23:16:51 +0530

    adds AuthUtils to access information about Authenticated User

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItems,Long> {
 
-    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    @Query("SELECT ci FROM CartItems ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     CartItems findCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
